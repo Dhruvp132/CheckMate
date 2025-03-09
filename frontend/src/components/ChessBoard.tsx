708a2c6c884fbc342b1ac0fa3571a2fea2f0d008
Dhruvp132 <106880373+Dhruvp132@users.noreply.github.com>
2025-03-09 18:04:58 +0530

    updated board

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -66,7 +66,7 @@ export const ChessBoard = ({ chess, board, socket, setBoard, color, roomId }: {
                             })
                         }
 
-                    }} key={j} className={`w-16 h-16 ${(i+j)%2 === 0 ? 'bg-green-500' : 'bg-slate-500'}`}>
+                    }} key={j} className={`w-16 h-16 ${(i+j)%2 === 0 ? 'bg-white-500' : 'bg-slate-500'}`}>
                         <div className="w-full justify-center flex h-full">
                             <div className="h-full justify-center flex flex-col">
                                 {square ? <img className="w-4" src={`/${square?.color === "b" ? square?.type : `${square?.type?.toUpperCase()} copy`}.png`} /> : null} 
